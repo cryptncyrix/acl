a7d0edef879c51572824f123e56c9fd82a8008e1
Christian Kupka <Christian Kupka>
2019-01-29 17:54:46 +0100

    Create the RoleController

---
+++ Http/Controllers/RoleController.php
@@ -0,0 +1,82 @@
+<?php
+namespace cyrixbiz\acl\controller;
+
+class RoleController
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Role Controller
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+     * Create a new RoleController Instance
+     *
+     * @return void
+     */
+
+    public function __construct()
+    {
+    }
+
+
+    /*
+     * Display all Roles
+     */
+    public function index()
+    {
+    }
+
+    /*
+     * Create a Role
+     */
+
+    public function create()
+    {
+
+    }
+
+    /*
+     * Save the Role to Database
+     */
+
+    public function store()
+    {
+
+    }
+
+    /*
+     * Show the Role
+     */
+
+    public function show()
+    {
+    }
+
+    /*
+     * Edit the Role
+     */
+
+    public function edit()
+    {
+    }
+
+
+    /*
+     * Update the Role
+     */
+
+    public function update()
+    {
+    }
+
+    /*
+     * Destroy the Role
+     */
+
+    public function destroy()
+    {
+    }
+
+
+}
